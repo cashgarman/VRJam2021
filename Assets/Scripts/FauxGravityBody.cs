d159fa263cbaa20a27cb17b48b13c69920bedbbd
frogman27 <frogman27@gmail.com>
2021-07-30 16:19:12 -0400

    Lee 7-30 4:18pm
    Added FauxGravityBody Script and FauxGravityAttractor Script + a batch of toy prefabs

---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public FauxGravityAttractor attractor;
+    private Transform myTransform;
+
+   void Start()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(myTransform);
+    }
+}
