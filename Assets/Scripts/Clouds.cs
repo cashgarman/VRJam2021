3c6db27ba2efc1c05dce53996454a126edd89e7a
Cash Garman <cashgarman@hotmail.co.uk>
2021-07-31 19:08:52 -0700

    Clouds finished.

--- Assets/Scripts/Clouds.cs
+++ Assets/Scripts/Clouds.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Clouds : MonoBehaviour
 {
@@ -6,14 +9,32 @@ public class Clouds : MonoBehaviour
     [SerializeField] private GameObject _lowLevelCloudPrefab;
     [SerializeField] private Transform _lowLevelCloudContainer;
     [SerializeField] private float _lowLevelCloudHeight = 10f;
+    
+    private readonly List<GameObject> _clouds = new List<GameObject>();
 
     void Start()
     {
+        SpawnClouds();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+            SpawnClouds();
+    }
+
+    private void SpawnClouds()
+    {
+        foreach(var existingCloud in _clouds)
+            Destroy(existingCloud.gameObject);
+        
         // Add the low level clouds
         for (var i = 0; i < _numLowLevelClouds; ++i)
         {
             var cloud = Instantiate(_lowLevelCloudPrefab, _lowLevelCloudContainer);
             cloud.transform.position = Random.onUnitSphere * _lowLevelCloudHeight;
+            cloud.transform.up = (cloud.transform.position - transform.position).normalized;
+            _clouds.Add(cloud);
         }
     }
 }
