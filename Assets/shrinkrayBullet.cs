bef4a997408ed1d40d428c668dd36ed07aa6c683
kristenmontesano <62899236+kristenmontesano@users.noreply.github.com>
2021-08-01 12:01:41 -0500

    Animated skybox + working raygun

---
+++ Assets/shrinkrayBullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shrinkrayBullet : MonoBehaviour
+{
+    private TrailRenderer trailRenderer;
+    private List<Vector3> shrinkableNewVertices = new List<Vector3>();
+
+    private void Awake()
+    {
+        trailRenderer = this.GetComponent<TrailRenderer>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        trailRenderer.enabled = false;
+        Destroy(this);
+
+        Debug.Log("shrinkray hit " + other.collider.name);
+
+        if (other.gameObject.CompareTag("Shrinkable"))
+        {
+            Debug.Log(other.collider.name + " is shrinkable");
+
+            GameObject shrinkableObject = other.gameObject;
+
+            shrinkableObject.transform.localScale = shrinkableObject.transform.localScale / 2;
+
+            Debug.Log("shrinking done");
+        }
+    }
+}
