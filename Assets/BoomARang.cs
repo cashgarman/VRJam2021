8cb204c0f2bc46eba2f6c3feff8ae26ed75ee175
frogman27 <frogman27@gmail.com>
2021-08-01 18:05:18 -0400

    making sure im current

---
+++ Assets/BoomARang.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomARang : MonoBehaviour
+{
+    private bool flying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flying)
+        {
+            transform.RotateAround(transform.position, transform.up, Time.deltaTime * 1000f);
+        }
+    }
+
+    public void Thrown()
+    {
+        flying = true;
+    }
+
+    public void grabbed()
+    {
+        flying = false;
+    }
+}
