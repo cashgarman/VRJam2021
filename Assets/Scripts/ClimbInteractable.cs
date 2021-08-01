4e9d25ce192f99ccc4beb549bf680a345f62f2af
frogman27 <frogman27@gmail.com>
2021-08-01 15:26:44 -0400

    Pushing the rocket launch up

---
+++ Assets/Scripts/ClimbInteractable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ClimbInteractable : XRBaseInteractable
+{
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        base.OnSelectEntered(interactor);
+    }
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+    }
+}
