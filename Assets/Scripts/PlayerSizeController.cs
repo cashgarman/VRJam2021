2b0a95794a984b306eeb37e7af227fcca169c2d6
Cash Garman <cashgarman@hotmail.co.uk>
2021-07-31 23:55:33 -0700

    Scaling reticles.

--- Assets/Scripts/PlayerSizeController.cs
+++ Assets/Scripts/PlayerSizeController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -13,7 +14,9 @@ public class PlayerSizeController : MonoBehaviour
     [SerializeField] private float _maxRayEndPointDistance = 1000f;
     [SerializeField] private float _maxRayWidth = 0.75f;
     [SerializeField] private float _minScale = 0.0001f;
-    
+    [SerializeField] private float _rayWidth = 8f;
+    [SerializeField] private float _maxReticleScale = 1f;
+
     private bool _leftHeld;
     private bool _rightHeld;
     private bool _resizing;
@@ -21,14 +24,20 @@ public class PlayerSizeController : MonoBehaviour
     private Vector3 _resizeStartScale;
     private Vector3 _initialScale;
     private SphericalContinuousMoveProvider _continuousMovement;
-    [SerializeField] private float _rayWidth = 8f;
+    private float _scaleFactor;
+
+    public List<TeleportReticle> Reticules { get; set; } = new List<TeleportReticle>();
 
     private void Awake()
     {
         _initialScale = transform.localScale;
         _continuousMovement = GetComponent<SphericalContinuousMoveProvider>();
-        UpdateRay(transform.localScale.magnitude / _maxScale);
-        UpdateMovementSpeed(transform.localScale.magnitude / _maxScale);
+        
+        _scaleFactor = transform.localScale.magnitude / _maxScale;
+        
+        // Initialize the ray and movement scale 
+        UpdateRay();
+        UpdateMovementSpeed();
     }
 
     public void OnLeftResizeActivated(InputAction.CallbackContext context)
@@ -54,12 +63,10 @@ public class PlayerSizeController : MonoBehaviour
             _resizing = true;
             _startingDistance = Vector3.Distance(_leftController.localPosition, _rightController.localPosition) / _maxControllerSpread;
             _resizeStartScale = transform.localScale;
-            Debug.Log($"Started resizing with controller distance: {_startingDistance}");
         }
         else if (_resizing && (!_leftHeld || !_rightHeld))
         {
             _resizing = false;
-            Debug.Log("Stopped resizing");
         }
 
         if (_resizing)
@@ -70,36 +77,47 @@ public class PlayerSizeController : MonoBehaviour
             // MATH!
             var scalePercent = (_startingDistance - distance) / _maxControllerSpread + 1f;
             transform.localScale = Vector3.Max(_resizeStartScale * scalePercent, _minScale * Vector3.one);
-            var scaleFactor = transform.localScale.magnitude / _maxScale;
+            _scaleFactor = transform.localScale.magnitude / _maxScale;
 
             // Update the player's movement speed
-            UpdateMovementSpeed(scaleFactor);
+            UpdateMovementSpeed();
 
             // Update the ray
-            UpdateRay(scaleFactor);
+            UpdateRay();
+            
+            // Update the reticules
+            UpdateReticles();
         }
     }
 
-    private void UpdateMovementSpeed(float scaleFactor)
+    private void UpdateMovementSpeed()
     {
-        _continuousMovement.MoveSpeed = Mathf.Lerp(0f, _continuousMovement.moveSpeed, scaleFactor);
+        _continuousMovement.MoveSpeed = Mathf.Lerp(0f, _continuousMovement.moveSpeed, _scaleFactor);
     }
 
-    private void UpdateRay(float scaleFactor)
+    private void UpdateRay()
     {
         // Scale down the ray interactor and ray line renderer width
         foreach (var ray in _rays)
         {
-            ray.endPointDistance = Mathf.Lerp(_maxRayEndPointDistance, 0f, scaleFactor);
-            ray.controlPointDistance = Mathf.Lerp(_maxRayEndPointDistance / 2f, 0f, scaleFactor);
-            ray.controlPointHeight = Mathf.Lerp(_maxRayEndPointDistance / 4f, 0f, scaleFactor);
+            ray.endPointDistance = Mathf.Lerp(_maxRayEndPointDistance, 0f, _scaleFactor);
+            ray.controlPointDistance = Mathf.Lerp(_maxRayEndPointDistance / 2f, 0f, _scaleFactor);
+            ray.controlPointHeight = Mathf.Lerp(_maxRayEndPointDistance / 4f, 0f, _scaleFactor);
         }
 
         foreach (var lineVisual in _lineVisuals)
         {
-            lineVisual.lineLength = Mathf.Lerp(0f, _maxRayEndPointDistance, scaleFactor);
-            lineVisual.widthCurve.keys[0].value = Mathf.Lerp(0f, _rayWidth, scaleFactor);
-            lineVisual.widthCurve.keys[1].value = Mathf.Lerp(0f, _rayWidth, scaleFactor);
+            lineVisual.lineLength = Mathf.Lerp(0f, _maxRayEndPointDistance, _scaleFactor);
+            lineVisual.widthCurve.keys[0].value = Mathf.Lerp(0f, _rayWidth, _scaleFactor);
+            lineVisual.widthCurve.keys[1].value = Mathf.Lerp(0f, _rayWidth, _scaleFactor);
+        }
+    }
+
+    private void UpdateReticles()
+    {
+        foreach (var reticle in Reticules)
+        {
+            reticle.SetScale(_scaleFactor);
         }
     }
 
@@ -108,5 +126,16 @@ public class PlayerSizeController : MonoBehaviour
         GUILayout.Label($"Player local scale: {transform.localScale}");
         GUILayout.Label($"Player world scale: {transform.lossyScale}");
         GUILayout.Label($"Approximate player height (Unity units): {GetComponent<CapsuleCollider>().height * transform.lossyScale}");
+    }
+
+    public void AddReticle(TeleportReticle reticle)
+    {
+        Reticules.Add(reticle);
+        reticle.SetScale(_scaleFactor);
+    }
+
+    public void RemoveReticle(TeleportReticle reticle)
+    {
+        Reticules.Remove(reticle);
     }
 }
