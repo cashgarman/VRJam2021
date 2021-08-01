f54a12cfd4481d8b4e93164fb224db3f4b5e0f5b
kristenmontesano <62899236+kristenmontesano@users.noreply.github.com>
2021-07-31 19:15:59 -0500

    TELEPORT WORKING! And hands.

--- Assets/Scripts/PlayerTeleportation.cs
+++ Assets/Scripts/PlayerTeleportation.cs
@@ -7,13 +7,30 @@ public class PlayerTeleportation : MonoBehaviour
 	public XRController _rightController;
 	
 	public InputHelpers.Button teleportRayShowButton;
-	
+
+	public float activationThreshold = 0.1f;
+
 	private void Update()
 	{
 		// Show and hide the teleport 'hands' based on the controller inputs
-		var left = InputHelpers.IsPressed(_leftController.inputDevice, teleportRayShowButton, out _, .1f);
-		
-		_leftController.gameObject.SetActive(left);
-		_rightController.gameObject.SetActive(_rightController.inputDevice.IsPressed(teleportRayShowButton, out _, .1f));
+		//var left = InputHelpers.IsPressed(_leftController.inputDevice, teleportRayShowButton,out ///_, .1f);
+		//
+		//_leftController.gameObject.SetActive(left);
+		//_rightController.gameObject.SetActive(_rightController.inputDevice.IsPressed//(teleportRayShowButton, out _, .1f));
+
+		if (_leftController)
+		{
+			_leftController.gameObject.SetActive(CheckIfActivated(_leftController));
+		}
+
+        if (_rightController){
+			_rightController.gameObject.SetActive(CheckIfActivated(_rightController));
+		}
 	}
-}
+
+	public bool CheckIfActivated(XRController controller)
+	{
+		InputHelpers.IsPressed(controller.inputDevice, teleportRayShowButton, out bool isActivated, activationThreshold);
+		return isActivated;
+	}
+}		
