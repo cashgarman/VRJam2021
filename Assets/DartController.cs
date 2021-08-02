173dfab1bc3a62b069104c4a3333d6d6c0c960fb
kristenmontesano <62899236+kristenmontesano@users.noreply.github.com>
2021-08-01 20:46:52 -0500

    More sounds - apk build version

--- Assets/DartController.cs
+++ Assets/DartController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class DartController : MonoBehaviour
 {
+
+    public AudioSource audioSource;
+    public AudioClip audioClip;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "DartBoard")
         {
             GetComponent<Rigidbody>().isKinematic = true;
             Achievements.Award("BullsEye");
+            audioSource.PlayOneShot(audioClip);
         }
     }
 }
